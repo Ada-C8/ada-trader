64360dd07ddef38de6725826ebfdeae6230754c9
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-12-11 15:37:33 -0800

    implement Quote#buy and Quote#sell functions

--- src/models/quote.js
+++ src/models/quote.js
@@ -8,10 +8,14 @@ const Quote = Backbone.Model.extend({
 
   buy() {
     // Implement this function to increase the price by $1.00
+    const newPrice = (this.get('price') + 1);
+    this.set('price', newPrice);
   },
 
   sell() {
     // Implement this function to decrease the price by $1.00
+    const newPrice = (this.get('price') - 1);
+    this.set('price', newPrice);
   },
 });
 
