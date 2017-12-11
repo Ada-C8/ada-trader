cc1038e1926d527069b379d069729b1b4d066480
averikitsch <aekitsch@gmail.com>
2017-12-11 15:51:41 -0800

    buy and sell history

--- src/views/quote_view.js
+++ src/views/quote_view.js
@@ -1,10 +1,14 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
 // import quote from '../models/quote';
 
 const QuoteView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template;
     this.listenTo(this.model, 'change', this.render);
+
+    this.tradeTemplate = _.template($('#trade-template').html());
   },
   render() {
     const compiledTemplate = this.template(this.model.toJSON());
@@ -12,7 +16,18 @@ const QuoteView = Backbone.View.extend({
     return this;
   },
   events: {
-
+    'click button.btn-buy': 'buyQuote',
+    'click button.btn-sell': 'sellQuote',
+  },
+  buyQuote: function() {
+    this.model.buy();
+    const addBuy = $.extend(this.model.toJSON(), {buy: true});
+    $('#trades').prepend(this.tradeTemplate(addBuy));
+  },
+  sellQuote: function() {
+    this.model.sell();
+    const addSell = $.extend(this.model.toJSON(), {buy: false});
+    $('#trades').prepend(this.tradeTemplate(addSell));
   },
 });
 
