454ab4f0a407a17461ea303180298221f2d5bf0d
Isaac Del Rosario <cecdelr@gmail.com>
2017-12-11 15:42:19 -0800

    created view files for Quote and QuoteList

---
+++ src/views/quote_list_view.js
@@ -0,0 +1,7 @@
+import Backbone from 'backbone'
+import _ from 'underscore';
+import Quote from '../models/quote';
+
+const QuoteListView = Backbone.View.extend({
+
+});
---
+++ src/views/quote_view.js
@@ -0,0 +1,6 @@
+import Backbone from 'backbone'
+import Quote from '../models/quote';
+
+const QuoteView = Backbone.View.extend({
+
+});
