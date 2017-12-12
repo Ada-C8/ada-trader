1b29b3620749c706c6b1c3dac468c3ea0d3a3d8d
Rebecca Bergena <rbergena@gmail.com>
2017-12-12 15:19:51 -0800

    add quote list view and make buy and sell buttons change quote's market price

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import 'foundation-sites/dist/foundation.css';
 import 'css/app.css';
 
 import $ from 'jquery';
+import _ from 'underscore';
 
 import Simulator from 'models/simulator';
 import QuoteList from 'collections/quote_list';
+
+import Quote from './models/quote';
+// import TaskList from './collections/task_list';
+import QuoteView from './views/quote_view';
+// import TaskListView from './views/task_list_view';
+import QuoteListView from './views/quote_list_view';
+
 
 const quoteData = [
   {
@@ -25,11 +33,27 @@ const quoteData = [
   },
 ];
 
+const quoteList = new QuoteList();
+let quoteTemplate;
+
 $(document).ready(function() {
+  quoteTemplate = _.template($('#quote-template').html());
+
   const quotes = new QuoteList(quoteData);
   const simulator = new Simulator({
     quotes: quotes,
   });
 
+  // Tasklistview will encompass the main tag
+  const quoteListView = new QuoteListView({
+    el: 'main',
+    model: quotes,
+    template: quoteTemplate,
+  });
+
+  quoteListView.render();
+
+  // wave 3
+  // any time the stock "changes" change event on model
   simulator.start();
 });
---
+++ src/views/quote_list_view.js
@@ -0,0 +1,54 @@
+import Backbone from 'backbone';
+import QuoteView from '../views/quote_view'
+
+import Quote from '../models/quote';
+
+const QuoteListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render() {
+    this.$('#quotes').empty();
+        console.log('in quotelistview render');
+    this.model.each((quote) => {
+      const quoteView = new QuoteView({
+        model: quote,
+        template: this.template,
+        tagName: 'li',
+        // TODO: check for styling
+        className: 'quote',
+      });
+      console.log('in quotelistview render');
+      this.$('#quotes').append(quoteView.render().$el);
+
+    });
+    return this;
+  }
+});
+// const TaskListView = Backbone.View.extend({
+//   initialize(params) {
+//     this.template = params.template;
+//     this.listenTo(this.model, 'update', this.render);
+//   },
+//   render() {
+//     // Clear the unordered list
+//     this.$('#todo-items').empty();
+//     // Iterate through the list rendering each Task
+//     this.model.each((task) => {
+//       // Create a new TaskView with the model & template
+//       const taskView = new TaskView({
+//         model: task,
+//         template: this.template,
+//         tagName: 'li',
+//         className: 'task',
+//       });
+//       // Then render the TaskView
+//       // And append the resulting HTML to the DOM.
+//       this.$('#todo-items').append(taskView.render().$el);
+//     });
+//     return this;
+//   }
+// });
+
+export default QuoteListView;
