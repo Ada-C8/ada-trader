22044979fbeef905951823eb7afee1a8bd731a1c
Amy Cash <amymcash@gmail.com>
2017-12-11 16:37:51 -0800

    Create Views

---
+++ src/views/quote_list_view.js
@@ -0,0 +1,16 @@
+import Backbone from 'backbone';
+
+import QuoteView from './quote_view';
+import Quote from '../models/quote';
+
+const QuoteListView = Backbone.View.extend({
+
+
+
+
+
+
+
+}); //end quotelistview
+
+export default QuoteListView;
---
+++ src/views/quote_view.js
@@ -0,0 +1,22 @@
+import Backbone from 'backbone';
+import Quote from '../models/quote';
+
+
+const QuoteView = Backbone.View.extend({
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //end of Quote View
+
+
+export default QuoteView;
