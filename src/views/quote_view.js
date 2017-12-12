16595a1f821e3ab4194e74ef4bd6f49a18f1d634
Andrea Chen <andreachen.ac@gmail.com>
2017-12-11 16:18:20 -0800

    Create quote_view.js. Add preliminary functions - initialize, render, and stubs for events object and corresponding functions

---
+++ src/views/quote_view.js
@@ -0,0 +1,34 @@
+import Backbone from 'backbone';
+import Quote from '../models/quote';
+
+const QuoteView = BackBone.View.extend({
+
+  initialize(params) {
+    this.template = params.template;
+
+    this.listenTo(this.model, 'change', this.render);
+  },
+
+  render() {
+    const compiledTemplate = this.template(thismodel.toJSON());
+
+    this.$el.html(compiledTemplate);
+
+    ////some other stuff here?
+
+    return this;
+  }
+
+  events: {
+    //listen for buy and sell buttons
+  }
+
+  //buy method
+
+  //sell method
+
+
+
+})
+
+export default QuoteView;
