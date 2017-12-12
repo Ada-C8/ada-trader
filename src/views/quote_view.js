03e981c5107b493995f82c505f51ee6b3f5da7a7
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-12-12 12:26:45 -0800

    event handling logic

--- src/views/quote_view.js
+++ src/views/quote_view.js
@@ -4,8 +4,28 @@ import Quote from '../models/quote';
 const QuoteView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template,
-    this.listenTo(this.model, 'change', this.render) // when a model changes, it will re-render 
+    this.listenTo(this.model, 'change', this.render) // when a model changes, it will re-render
+  },
+  render() {
+    const compiledTemplate = this.template(this.model.toJSON());
+
+    this.$el.html(compiledTemplate);
+
+    return this;
+  },
+  events: {
+    'click button.btn-buy': 'buy',
+    'click button.btn-sell': 'sell',
+  },
+  buy(event) {
+    console.log(event);
+    this.model.buy();
+  },
+  sell(event) {
+    console.log(event);
+    this.model.sell();
   }
+
 });
 
 export default QuoteView;
