7213975239c99002d962350dbee5ae582db7c090
nkiru onwuneme <generalhrh@gmail.com>
2017-12-12 15:33:43 -0800

    add quote view

---
+++ src/views/quote_view.js
@@ -0,0 +1,15 @@
+import Backbone from 'backbone';
+import Quote from '../models/quote';
+
+const QuoteView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+  },
+  render() {
+    const compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+    return this;
+  },
+});
+
+export default QuoteView;
