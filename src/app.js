eb3c03b96257cb2fc3c2f8e3e632afb2a63110f4
Maria McGrew <mcgmar1@outlook.com>
2017-12-12 15:35:38 -0800

    added views for user to see list of quotes in the quotes section

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import 'foundation-sites/dist/foundation.css';
 import 'css/app.css';
 
 import $ from 'jquery';
+import _ from 'underscore';
 
 import Simulator from 'models/simulator';
-import QuoteList from 'collections/quote_list';
+
+import Quote from './models/quote';
+import QuoteList from './collections/quote_list';
+
+import QuoteView from './views/quote_view';
+import QuoteListView from './views/quote_list_view';
+
+const quoteList = new QuoteList();
 
 const quoteData = [
   {
@@ -25,11 +33,21 @@ const quoteData = [
   },
 ];
 
+
 $(document).ready(function() {
   const quotes = new QuoteList(quoteData);
   const simulator = new Simulator({
     quotes: quotes,
   });
 
+  const quoteListView = new QuoteListView({
+    model: quotes,
+    template: _.template($('#quote-template').html()),
+    el: 'main'
+  });
+
+  quoteListView.render();
+
   simulator.start();
+
 });
---
+++ src/views/quote_list_view.js
@@ -0,0 +1,25 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import QuoteView from '../views/quote_view';
+import Quote from '../models/quote';
+
+const QuoteListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render() {
+    this.$('#quotes').empty();
+    this.model.each((quote) => {
+      const quoteView = new QuoteView({
+        model: quote,
+        template: this.template,
+      });
+
+      this.$('#quotes').append(quoteView.render().$el);
+    });
+    return this;
+  }, // end of render
+}); //end of QuoteListView
+
+export default QuoteListView
