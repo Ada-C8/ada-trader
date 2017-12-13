a0dd90a8fd8134407ef9245757c9b61292e9ffe7
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-12-13 15:16:32 -0800

    More fully fleshed out tests.

--- spec/models/order_spec.js
+++ spec/models/order_spec.js
@@ -4,17 +4,54 @@ describe('Order spec', () => {
   let order;
 
   describe('validations', () => {
-    it('increases the price by $1.00', () => {
+    it('creates a quote', () => {
       order = new Order({
         symbol: 'HELLO',
-        price: 100.00,
+        targetPrice: 100.00,
       });
 
-      const startPrice = quote.get('price');
+      expect(order).toBeDefined();
+    });
 
-      quote.buy();
+    it('requires a symbol', () => {
+      order = new Order({
+        targetPrice: 100.00,
+      });
 
-      expect(quote.get('price')).toEqual(startPrice + 1.00);
+      expect(expect(order).toBeDefined()).toEqual(undefined);
+    });
+
+    it('requires a targetPrice', () => {
+      order = new Order({
+        symbol: 'HELLO',
+      });
+
+      expect(expect(order).toBeDefined()).toEqual(undefined);
+    });
+
+    it('requires a targetPrice to be a number', () => {
+      order = new Order({
+        symbol: 'HELLO',
+        targetPrice: 'cats',
+      });
+
+      expect(expect(order).toBeDefined()).toEqual(undefined);
+    });
+
+    it('will not let you buy if quote price is lower than target price', () => {
+      order = new Order({
+        symbol: 'HELLO',
+      });
+
+      expect(expect(order).toBeDefined()).toEqual(undefined);
+    });
+
+    it('will not let you sell if quote price is higher than target price', () => {
+      order = new Order({
+        symbol: 'HELLO',
+      });
+
+      expect(expect(order).toBeDefined()).toEqual(undefined);
     });
 
   });
