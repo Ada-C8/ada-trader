fca510c1c6f6879a5dd16d43b7f93cbec8d33951
Canaan West <canaan.west@gmail.com>
2017-12-13 15:38:51 -0800

    modifying export statement for openquote

--- src/models/open_quote.js
+++ src/models/open_quote.js
@@ -11,7 +11,7 @@ const OpenQuote = Backbone.Model.extend({
   // '.order-entry-form'
   //2. must evaluate whether or not a price meets acceptability standards (can't be higher than current price for sell or lower than current price for buy--else not in best interest of trader)
   //3. must have a listener for when the price is a specified amount; must trigger a buy/sell event;
-  //4. which means that buy/sell must have an event handler for listening to this model. 
+  //4. which means that buy/sell must have an event handler for listening to this model.
 });
 
-export default Quote;
+export default OpenQuote;
