edd6b07ad97ca0540c054cfcaaae5bbada6e5deb
Maria McGrew <mcgmar1@outlook.com>
2017-12-12 18:07:35 -0800

    tests passing for buy and sell functions in quote.js

--- src/models/quote.js
+++ src/models/quote.js
@@ -7,10 +7,12 @@ const Quote = Backbone.Model.extend({
   },
 
   buy() {
+     this.set('price', this.get('price') + 1)
     // Implement this function to increase the price by $1.00
   },
 
   sell() {
+    this.set('price', this.get('price') - 1)
     // Implement this function to decrease the price by $1.00
   },
 });
