29cbf73c9fe9796d4d90e69555c4a77e4fdf2a6f
Angela Wilson <wilsonange@gmail.com>
2017-12-13 10:07:31 -0800

    made quote_list_view initialize

--- src/views/quote_list_view.js
+++ src/views/quote_list_view.js
@@ -1,3 +1,19 @@
 import Backbone from 'backbone';
 
 import QuoteView from './quote_view';
+import Quote from '../models/task';
+import CurrentQuoteView from './current_selected_view';
+
+const QuoteListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render() {
+    this.$('#quotes').empty();
+  }
+  events: {
+    'click #add-'
+  }
+})
