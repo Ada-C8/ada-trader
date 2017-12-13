826c14ce1b53fa84942d05c8b402f4c9a5be571f
Kayla Ecker <kaylajayecker@gmail.com>
2017-12-13 10:19:55 -0800

    rendering for quote list view

--- src/views/quote_list_view.js
+++ src/views/quote_list_view.js
@@ -2,6 +2,27 @@ import Backbone from 'backbone';
 import Quote from '../models/quote';
 import QuoteView from './quote_view';
 
+const QuoteListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
 
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render() {
+    this.$('#quotes').empty();
+
+    this.model.each((quote) => {
+      const quoteView = new QuoteView({
+        model: quote,
+        template: this.template,
+        tagName: 'li',
+        className: 'quote',
+      });
+
+      this.$('#quotes').append(quoteView.render().$el);
+    });
+    return this;
+  },
+});
 
 export default QuoteListView;
