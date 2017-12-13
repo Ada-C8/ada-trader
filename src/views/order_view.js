3a4b1b1615f8efa509c41d75f3979c0ce8e9c80d
averikitsch <aekitsch@gmail.com>
2017-12-13 12:22:47 -0800

    added listener to order view

--- src/views/order_view.js
+++ src/views/order_view.js
@@ -7,11 +7,19 @@ const OrderView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template;
     this.orderTemplate = _.template($('#order-template').html());
+    this.listenTo(this.model, 'orderMe', this.deleteOrder);
   },
   render() {
     const compiledTemplate = this.template(this.model.toJSON());
     this.$el.html(compiledTemplate);
     return this;
+  },
+  events: {
+    'click button.btn-cancel': 'deleteOrder',
+  },
+  deleteOrder() {
+    this.model.destroy();
+    this.remove();
   },
 });
 
