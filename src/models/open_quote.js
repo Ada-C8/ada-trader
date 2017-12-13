7973bbef7ca264516a01502a6efdaef2eae6b8ca
Canaan West <canaan.west@gmail.com>
2017-12-13 15:36:50 -0800

    adding openquote model

---
+++ src/models/open_quote.js
@@ -0,0 +1,13 @@
+import Backbone from 'backbone';
+
+const OpenQuote = Backbone.Model.extend({
+  defaults: {
+    symbol: 'UNDEF',
+    price: 0.00
+  },
+
+  //functionality open quotes need:
+  
+});
+
+export default Quote;
