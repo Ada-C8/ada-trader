c9c6eb9995aa19fd5e4a1d11dd622214527ba51c
Sara Frandsen <sara.frandsen@gmail.com>
2017-12-12 22:00:30 -0800

    add to buy/sell functions to 'get' symbols and prices

--- src/views/quote_view.js
+++ src/views/quote_view.js
@@ -1,8 +1,10 @@
 import Backbone from 'backbone';
+import Trade from '../models/trade';
 
 const QuoteView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template;
+    this.trades = params.trades;
     this.listenTo(this.model, "change", this.render);
   },
   render() {
@@ -18,10 +20,22 @@ const QuoteView = Backbone.View.extend({
   buyQuote() {
     this.model.buy();
     console.log('bought!');
+    const trade = new Trade({
+      symbol: this.model.get('symbol'),
+      buy: true,
+      price: this.model.get('price'),
+    });
+    this.trades.add(trade);
   },
   sellQuote() {
     this.model.sell();
     console.log('sold!');
+    const trade = new Trade({
+      symbol: this.model.get('symbol'),
+      buy: false,
+      price: this.model.get('price'),
+    });
+    this.trades.add(trade);
   },
 });
 
