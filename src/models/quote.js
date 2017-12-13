0730503c460e35d5c2d01730a44c12b4446caff7
Kayla Ecker <kaylajayecker@gmail.com>
2017-12-13 14:13:35 -0800

    buy/sell trigger trade

--- src/models/quote.js
+++ src/models/quote.js
@@ -7,11 +7,21 @@ const Quote = Backbone.Model.extend({
   },
 
   buy() {
+    this.trigger('trade', {
+      buy: true,
+      price: this.get('price'),
+      symbol: this.get('symbol')
+    });
     // Implement this function to increase the price by $1.00
     this.set('price', this.get('price') + 1.00);
   },
 
   sell() {
+    this.trigger('trade', {
+      buy: false,
+      price: this.get('price'),
+      symbol: this.get('symbol')
+    });
     // Implement this function to decrease the price by $1.00
     this.set('price', this.get('price') - 1.00);
   },
