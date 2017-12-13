b6c4a824e9359269ae9a776a25d82bfcdff29b87
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2017-12-13 12:03:50 -0800

    one quote with price and buttons showing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import 'foundation-sites/dist/foundation.css';
 import 'css/app.css';
 
 import $ from 'jquery';
+import _ from 'underscore';
 
 import Simulator from 'models/simulator';
 import QuoteList from 'collections/quote_list';
+import QuoteView from './views/quote_view';
 
 const quoteData = [
   {
@@ -25,11 +27,31 @@ const quoteData = [
   },
 ];
 
+// let quoteTemplate;
+const renderList = (quoteList) => {
+  // Clear the unordered list
+  const $quoteList = $('#quotes');
+  $quoteList.empty();
+
+  // quoteList.forEach((quote) => {
+    const quoteView = new QuoteView({
+      model: quoteList.at(0),
+      template: _.template($('#quote-template').html()),
+      tagName: 'li',
+      className: 'quote',
+    });
+    quoteView.render();
+    $quoteList.append(quoteView.render().$el);
+  // });
+};
+
 $(document).ready(function() {
   const quotes = new QuoteList(quoteData);
   const simulator = new Simulator({
     quotes: quotes,
   });
+
+  renderList(quotes);
 
   simulator.start();
 });
