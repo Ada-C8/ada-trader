f00795bc045a2779712895ef8c87e2b8f419fb2b
averikitsch <aekitsch@gmail.com>
2017-12-13 12:24:34 -0800

    add listener to order model

--- src/models/order.js
+++ src/models/order.js
@@ -1,7 +1,42 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
 
 const Order = Backbone.Model.extend({
+  initialize(attributes) {
+    this.buy = attributes.buy;
+    this.targetPrice = attributes.targetPrice;
+    this.quote = attributes.quote;
+    this.listenTo(this.quote, 'change', this.orderMe);
+  },
+  validate(attributes) {
+    const errors = {};
+    if (!attributes.targetPrice) {
+      errors['price'] = 'Invalid target price';
+    } else if (attributes.buy && attributes.targetPrice >= attributes.quote.get('price')) {
+      errors['price'] = 'Price higher than market price!';
+    } else if (!attributes.buy && attributes.targetPrice <= attributes.quote.get('price')) {
+      errors['price'] = 'Price lower than market price!';
+    }
 
+    if ( Object.keys(errors).length > 0 ) {
+      return errors;
+    } else {
+      return false;
+    }
+  },
+  orderMe() {
+    console.log('listen');
+    console.log(this.targetPrice);
+    console.log(this.quote.get('price'));
+    if (this.buy && this.targetPrice >= this.quote.get('price')) {
+      this.trigger('orderMe');
+      this.quote.buy();
+    } else if (!this.buy && this.targetPrice <= this.quote.get('price')) {
+      this.trigger('orderMe');
+      this.quote.sell();
+    }
+  },
 });
 
 export default Order;
