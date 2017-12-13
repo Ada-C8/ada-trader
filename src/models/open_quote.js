0331a3be6726a02ca972302a3c655872540960bc
Canaan West <canaan.west@gmail.com>
2017-12-13 15:38:30 -0800

    adding some pseudocode

--- src/models/open_quote.js
+++ src/models/open_quote.js
@@ -7,7 +7,11 @@ const OpenQuote = Backbone.Model.extend({
   },
 
   //functionality open quotes need:
-  
+  //1. must create a quote with information from
+  // '.order-entry-form'
+  //2. must evaluate whether or not a price meets acceptability standards (can't be higher than current price for sell or lower than current price for buy--else not in best interest of trader)
+  //3. must have a listener for when the price is a specified amount; must trigger a buy/sell event;
+  //4. which means that buy/sell must have an event handler for listening to this model. 
 });
 
 export default Quote;
