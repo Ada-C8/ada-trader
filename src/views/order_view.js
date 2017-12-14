f3bf284a001204e6f10eedcc4682ffb5f5f1bf7c
nkiru onwuneme <generalhrh@gmail.com>
2017-12-14 10:17:21 -0800

    add order view

---
+++ src/views/order_view.js
@@ -0,0 +1,9 @@
+import Backbone from 'backbone';
+import Order from '../models/order';
+
+const OrderView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+    this.listenTo(this.model, 'change', this.render);
+  },
+});
