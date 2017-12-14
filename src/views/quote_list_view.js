99f95d414ff02760da046a44209273ef42ea3126
murog <guillermina.muro@gmail.com>
2017-12-13 18:58:07 -0800

    create quote view, quote list view, and rendered quotes to #quotes

---
+++ src/views/quote_list_view.js
@@ -0,0 +1,30 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+
+import QuoteView from '../views/quote_view';
+import Quote from '../models/quote';
+
+const QuoteListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render() {
+    this.$('#quotes').empty();
+    this.model.each((quote) => {
+      const quoteView = new QuoteView({
+        model: quote,
+        template: this.template,
+        tagName: 'li',
+        className: 'quote',
+      });
+      this.$('#quotes').append(quoteView.render().$el);
+    });
+    return this;
+  },
+  events: {
+
+  },
+});
+
+export default QuoteListView;
