dd184657cfbc286a591084912e963f08980ae256
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-12-15 11:46:09 -0800

    Add order validation testing

--- spec/models/order_spec.js
+++ spec/models/order_spec.js
@@ -3,5 +3,32 @@ import _ from 'underscore';
 import Backbone from 'backbone';
 
 describe('Order spec', () => {
-  
+  let order;
+  let eventBus;
+  beforeEach(() => {
+    eventBus = {};
+    eventBus = _.extend(eventBus, Backbone.Events);
+    order = new Order({
+      symbol: 'VALID',
+      buy: true,
+      targetPrice: '100',
+      bus: eventBus,
+    });
+  });
+
+  describe('Validations', () => {
+    it('Will not allow a blank symbol', () => {
+      order.set('symbol', '');
+
+      expect(order.isValid()).toEqual(false);
+      expect(order.validationError).toEqual({symbol: ['Symbol is required']});
+    });
+
+    it('Will not allow a blank or NaN targetPrice', () => {
+      order.set('targetPrice', '');
+
+      expect(order.isValid()).toEqual(false);
+      expect(order.validationError).toEqual({price: ['Invalid target price']});
+    });
+  });
 });
