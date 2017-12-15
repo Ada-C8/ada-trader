bdd06062a8f5f6e5d0e741943ec1ddab6f8bca9a
Isaac Del Rosario <cecdelr@gmail.com>
2017-12-15 12:22:06 -0800

    added Order attribute validations and tests

--- src/models/order.js
+++ src/models/order.js
@@ -1,11 +1,58 @@
 import Backbone from 'backbone';
+import Quote from 'models/quote';
 
 const Order = Backbone.Model.extend({
-  initialize(){
-  },
   defaults:{
     symbol: 'UNDEF',
     targetPrice: 0.00
+    //buy: true or false
+    //quote: assigned at render in OrderListView
+  },
+
+  validate(attributes){
+    const errors = {};
+
+    if (attributes.targetPrice <= 0.00) {
+      errors['targetPrice'] = ['Target price must be a valid decimal above 0.'];
+    }
+
+    if (attributes.symbol === 'UNDEF' || attributes.symbol === undefined){
+      errors['symbol'] = ['Symbol is undefined.'];
+    }
+
+    if (attributes.quote === undefined){
+      errors['quote'] = ['Order has to have an associated quote.'];
+    }
+
+    if (attributes.buy === undefined){
+      errors['buy'] = ["Order has to know whether it's buying/selling a quote."]
+    }
+
+    // attribute type validations
+
+    if(typeof attributes.targetPrice !== 'number') {
+      errors[''] = ["order's targetPrice is not of type Number"];
+    }
+
+    if(typeof attributes.symbol !== 'string') {
+      errors[''] = ["order's symbol is not of type String"];
+    }
+
+    if(typeof attributes.buy !== 'boolean') {
+      errors[''] = ["order's buy is not of type Boolean"];
+    }
+
+    if(!attributes.quote instanceof Quote) {
+      errors[''] = ["order's quote is not an instance of Quote"];
+    }
+
+    if ( Object.keys(errors).length > 0 ) {
+      return errors;
+    } else {
+      return false;
+    }
+
+    // "check current price of quote" validations
   },
 
   buy(){
@@ -25,7 +72,7 @@ const Order = Backbone.Model.extend({
       quote.buy();
       this.destroy();
     // if sell, check if the price is higher or equal
-  } else if(this.get('buy') === false && (this.get('targetPrice') <= quote.get('price'))){
+    } else if(this.get('buy') === false && (this.get('targetPrice') <= quote.get('price'))){
       console.log('in sold at quotePriceCheck at order model');
       quote.sell();
       this.destroy();
