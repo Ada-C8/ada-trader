435058ac1a54498241fea14eef3d66eefeeb936c
Shaunna <skwiens@gmail.com>
2017-12-16 15:09:33 -0800

    refactor tests

--- spec/models/order_spec.js
+++ spec/models/order_spec.js
@@ -3,54 +3,44 @@ import Order from 'models/order';
 
 describe('Order spec', () => {
   let quote;
+  let order;
   beforeEach(() => {
     quote = new Quote({
       symbol: 'CLOTH',
       price: 100.00,
     });
+    order = new Order({
+      symbol: 'CLOTH',
+      quote: quote,
+      buy: true,
+      targetPrice: 100,
+    })
   });
 
   describe('Create a new Order', () => {
     it('creates a valid instance of an Order', () => {
-      const order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 80.00,
-        buy: true,
-        quote: quote,
-      })
+      order.set('targetPrice', 80.00);
+      order.set('buy', true);
 
       expect(order.isValid()).toEqual(true);
     })
 
     it('invalid order if price is not a number', () => {
-      const order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 'hello',
-        buy: false,
-        quote: quote,
-      })
+      order.set('targetPrice', 'hello');
 
       expect(order.isValid()).toEqual(false);
     })
 
     it('invalid buy order if price is higher than market', () => {
-      const order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 180.00,
-        buy: true,
-        quote: quote,
-      })
+      order.set('targetPrice', 180);
+      order.set('buy', true);
 
       expect(order.isValid()).toEqual(false);
     })
 
     it('invalid sell order if price is lower than market', () => {
-      const order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 8.00,
-        buy: false,
-        quote: quote,
-      })
+      order.set('targetPrice', 8);
+      order.set('buy', false);
 
       expect(order.isValid()).toEqual(false);
     })
@@ -58,62 +48,39 @@ describe('Order spec', () => {
 
   describe ('shouldTrade method', () => {
     it('returns "buy" if the trade should be bought', () => {
-      let order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 120.00,
-        buy: true,
-        quote: quote,
-      })
+
+      order.set('buy', true);
+      order.set('targetPrice', 120);
 
       expect(order.shouldTrade()).toEqual('buy');
 
-      order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 100.00,
-        buy: true,
-        quote: quote,
-      })
+      order.set('targetPrice', 100);
 
       expect(order.shouldTrade()).toEqual('buy');
     })
 
     it('returns "sell" if the trade should be sold', () => {
-      let order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 90.00,
-        buy: false,
-        quote: quote,
-      })
+
+      order.set('buy', false);
+      order.set('targetPrice', 90);
 
       expect(order.shouldTrade()).toEqual('sell');
 
-      order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 100.00,
-        buy: false,
-        quote: quote,
-      })
+      order.set('targetPrice', 100);
 
       expect(order.shouldTrade()).toEqual('sell');
 
     });
 
     it('returns false if no trade should be made', () => {
-      let order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 110.00,
-        buy: false,
-        quote: quote,
-      })
+
+      order.set('buy', false);
+      order.set('targetPrice', 110);
 
       expect(order.shouldTrade()).toEqual(false);
 
-      order = new Order({
-        symbol: 'CLOTH',
-        targetPrice: 90.00,
-        buy: true,
-        quote: quote,
-      })
+      order.set('buy', true);
+      order.set('targetPrice', 90);
 
       expect(order.shouldTrade()).toEqual(false);
 
