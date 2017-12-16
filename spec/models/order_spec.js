9d3cf1cf32f5b42dc92558872d2ac03ac4d3669e
Shaunna <skwiens@gmail.com>
2017-12-16 14:50:58 -0800

    tests for shouldTrade in order.js

--- spec/models/order_spec.js
+++ spec/models/order_spec.js
@@ -55,4 +55,68 @@ describe('Order spec', () => {
       expect(order.isValid()).toEqual(false);
     })
   });
+
+  describe ('shouldTrade method', () => {
+    it('returns "buy" if the trade should be bought', () => {
+      let order = new Order({
+        symbol: 'CLOTH',
+        targetPrice: 120.00,
+        buy: true,
+        quote: quote,
+      })
+
+      expect(order.shouldTrade()).toEqual('buy');
+
+      order = new Order({
+        symbol: 'CLOTH',
+        targetPrice: 100.00,
+        buy: true,
+        quote: quote,
+      })
+
+      expect(order.shouldTrade()).toEqual('buy');
+    })
+
+    it('returns "sell" if the trade should be sold', () => {
+      let order = new Order({
+        symbol: 'CLOTH',
+        targetPrice: 90.00,
+        buy: false,
+        quote: quote,
+      })
+
+      expect(order.shouldTrade()).toEqual('sell');
+
+      order = new Order({
+        symbol: 'CLOTH',
+        targetPrice: 100.00,
+        buy: false,
+        quote: quote,
+      })
+
+      expect(order.shouldTrade()).toEqual('sell');
+
+    });
+
+    it('returns false if no trade should be made', () => {
+      let order = new Order({
+        symbol: 'CLOTH',
+        targetPrice: 110.00,
+        buy: false,
+        quote: quote,
+      })
+
+      expect(order.shouldTrade()).toEqual(false);
+
+      order = new Order({
+        symbol: 'CLOTH',
+        targetPrice: 90.00,
+        buy: true,
+        quote: quote,
+      })
+
+      expect(order.shouldTrade()).toEqual(false);
+
+    });
+  });
 });
