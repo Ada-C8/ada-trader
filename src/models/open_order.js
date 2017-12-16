69c732d8f81ea0b9729b04e3d5b3017f50ed0abb
Rebecca Bergena <rbergena@gmail.com>
2017-12-15 22:46:58 -0800

    remove extraneous comments and completed todos from open_order model

--- src/models/open_order.js
+++ src/models/open_order.js
@@ -3,35 +3,21 @@ import Backbone from 'backbone';
 const OpenOrder = Backbone.Model.extend({
   initialize(attributes) {
   },
-  // TODO: validations
   validate(attributes) {
-    console.log('the quote');
-    console.log(attributes.quote);
     const errors = {};
+
     if (!attributes.targetPrice) {
       errors['target_price'] = ["Price cannot be blank."];
-      console.log('error')
-      // console.log(attributes.quote);
     }
-    // TODO: get CurrentMarketPrice from quote
-//     If the target price is blank OR is greater than or equal to the current market price:
-// That order is not created and an appropriate error message is displayed beneath the form
-    // if buy is true
-    // TODO: need to get quote.price at that moment in time
-    if (attributes.buy && attributes.targetPrice >= attributes.quote.get('price')) {
 
-    // if (attributes.buy && attributes.targetPrice >= CurrentMarketPrice) {
+    if (attributes.buy && attributes.targetPrice >= attributes.quote.get('price')) {
       errors['target_price'] = ["The price you listed for a buy order is greater than or equal to the current quote price."];
     }
-    if (!attributes.buy && attributes.targetPrice <= attributes.quote.get('price')) {
 
-    // if (attributes.buy && attributes.targetPrice >= CurrentMarketPrice) {
+    if (!attributes.buy && attributes.targetPrice <= attributes.quote.get('price')) {
       errors['target_price'] = ["The price you listed for a sell order is less than or equal to the current quote price."];
     }
-    // if (!attributes.task_name) {
-    //   errors['task_name'] = ["Task name is required"];
-    // }
-    //
+
     if ( Object.keys(errors).length > 0 ) {
       return errors;
     } else {
@@ -39,20 +25,5 @@ const OpenOrder = Backbone.Model.extend({
     }
   },
 });
-
-// Click Buy in the order entry form:
-// If the target price is blank OR is greater than or equal to the current market price:
-// That order is not created and an appropriate error message is displayed beneath the form
-// If the target price is less than to the current market price:
-// A new Buy order is added to the bottom of the open orders list, with the:
-// Symbol from the the form
-// Target price from the form
-// Click Sell in the order entry form:
-// If the target price is blank OR is less than or equal to the current market price:
-// That order is not created and an appropriate error message is displayed beneath the form
-// If the target price is greater than the current market price:
-// A new Sell order is added to the bottom of the open orders list, with the:
-// Symbol from the the form
-// Target price from the form
 
 export default OpenOrder;
