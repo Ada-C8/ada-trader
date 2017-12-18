b53a938c14227a32c6669912f7b9623f20858d7d
kee nam <nam.keeyeon@gmail.com>
2017-12-17 19:40:16 -0800

    Added empty order and order list models and views

---
+++ src/views/order_list_view.js
@@ -0,0 +1,11 @@
+import Backbone from 'backbone';
+import OrderView from './order_view';
+import Order from '../models/order';
+
+const OrderListView = Backbone.View.extend({
+  initialize(params){},
+  render(){},
+  events: {},
+});
+
+export default OrderListView;
---
+++ src/views/order_view.js
@@ -0,0 +1,10 @@
+import Backbone from 'backbone';
+import Order from '../models/order';
+
+const OrderView = Backbone.View.extend({
+  initialize(params){},
+  render(){},
+  events: {},
+});
+
+export default OrderView;
