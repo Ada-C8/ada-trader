0745a79f8a6f1610fc4b1a961d6a60b485f263d7
Sara C <srachandler@gmail.com>
2017-12-17 21:58:33 -0800

    added trade view

---
+++ src/views/trade_view.js
@@ -0,0 +1,18 @@
+import Backbone from 'backbone';
+import Quote from '../models/quote';
+
+const TradeView = Backbone.View.extend ({
+  initialize(params) {
+    this.template = params.template;
+  },
+
+  render() {
+    const compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+
+    return this;
+  },
+});
+
+
+export default TradeView;
