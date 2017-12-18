0c29ff4f3ca97df6f08d2a94a67c09e89063c2af
Rebecca Bergena <rbergena@gmail.com>
2017-12-18 04:48:50 -0800

    clean up extraneous comments in open order list view

--- src/views/open_order_list_view.js
+++ src/views/open_order_list_view.js
@@ -6,7 +6,6 @@ import OpenOrder from '../models/open_order';
 const OpenOrderListView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template;
-    // this.listenTo(this.model, 'update', this.render);
     this.bus = params.bus;
     this.listenTo(this.model, 'update', this.render);
     this.listenTo(this.bus, 'quote_symbols', this.symbolDropdown);
@@ -14,12 +13,6 @@ const OpenOrderListView = Backbone.View.extend({
 
     // list for current quote list
     this.listenTo(this.bus, 'current_quote_list', this.getQuoteList);
-
-    // when the quotelist changes, the checkOpenOrders function will check if orders should be purchased or sold
-    // this.listenTo(this.bus, 'updated_quote_list', this.checkOpenOrders);
-    // this.listenTo(this.bus, 'updated_quote_list', this.checkOpenOrders);
-
-
   },
   getQuoteList(quoteList) {
     this.quoteList = quoteList;
@@ -38,19 +31,14 @@ const OpenOrderListView = Backbone.View.extend({
         className: 'order',
         bus: this.bus,
       });
-      // render returns the taskview which (this) which allows you to append
+      // render returns the taskview (this) which allows you to append
       this.$('#orders').append(openOrderView.render().$el);
     });
     return this;
   },
-  // checkOpenOrders(quoteList) {
   checkOpenOrders(quote) {
-    console.log('in open orders');
-    // console.log(quoteList)
-    // var musketeers = friends.where({job: "Musketeer"});
 
     let openOrders = this.model.where({symbol: quote.get('symbol')});
-    console.log(openOrders);
     if (openOrders.length >= 1) {
       openOrders.forEach((openOrder) => {
         // if you are buying
@@ -61,7 +49,6 @@ const OpenOrderListView = Backbone.View.extend({
             buy: true,
           }
           this.bus.trigger('add_trade', tradeObject)
-          console.log(openOrder);
           // destroy purchased open order
           openOrder.destroy();
           // make sure quote is recognized as bought so price increases appropriately
@@ -73,9 +60,7 @@ const OpenOrderListView = Backbone.View.extend({
             symbol: openOrder.get('symbol'),
             buy: false,
           }
-
           this.bus.trigger('add_trade', tradeObject)
-          console.log(openOrder);
           // destroy purchased open order
           openOrder.destroy();
           // make sure quote is recognized as sold so price decreases appropriately
@@ -83,71 +68,15 @@ const OpenOrderListView = Backbone.View.extend({
         }
       });
     }
-  // console.log(openOrders);
-  // this.model.each((openOrder) => {
-  //   quoteList.each((quote) => {
-  //     // if you are buying
-  //     if (openOrder.get('buy') && openOrder.get('symbol') === quote.get('symbol')) {
-  //       console.log('order target price');
-  //       console.log(openOrder.get('targetPrice'));
-  //       console.log('quote price');
-  //       console.log(quote.get('price'));
-  //
-  //       if (openOrder.get('targetPrice') >= quote.get('price')) {
-  //         let tradeObject = {
-  //           price: openOrder.get('targetPrice'),
-  //           symbol: openOrder.get('symbol'),
-  //           buy: true,
-  //         }
-  //
-  //         this.bus.trigger('add_trade', tradeObject)
-  //         console.log(openOrder);
-  //         // destroy purchased open order
-  //         openOrder.destroy();
-  //         // make sure quote is recognized as bought so price increases appropriately
-  //         quote.buy();
-  //       }
-  //     }
-  //     else if ((!openOrder.get('buy')) && openOrder.get('symbol') === quote.get('symbol')) {
-  //         console.log('order target price');
-  //         console.log(openOrder.get('targetPrice'));
-  //         console.log('quote price');
-  //         console.log(quote.get('price'));
-  //
-  //         if (openOrder.get('targetPrice') <= quote.get('price')) {
-  //           let tradeObject = {
-  //             price: openOrder.get('targetPrice'),
-  //             symbol: openOrder.get('symbol'),
-  //             buy: false,
-  //           }
-  //
-  //           this.bus.trigger('add_trade', tradeObject)
-  //           console.log(openOrder);
-  //           // destroy purchased open order
-  //           openOrder.destroy();
-  //           // make sure quote is recognized as sold so price decreases appropriately
-  //           quote.sell();
-  //
-  //     }
-  //   }
-  //   });
-  // });
 },
 symbolDropdown(quotes) {
   quotes.forEach((quote) => {
-    // move to model??
-    console.log('in symbol dropdown');
-    // console.log(quote.get('symbol'));
-    // this.$('select[name=symbol]').append(`<option value="${quote.get('symbol')}">${quote.get('symbol')}</option>`)
     this.$('select[name=symbol]').append(`<option value="${quote}">${quote}</option>`)
-    // input[name=${field}]
-    // <option value="volvo">Volvo</option>
   });
 },
 events: {
   'click button.btn-buy': 'addBuyOpenOrder',
   'click button.btn-sell': 'addSellOpenOrder',
-
 },
 updateStatusMessageFrom(messageHash) {
   const $formErrors = this.$('.form-errors');
@@ -160,16 +89,15 @@ updateStatusMessageFrom(messageHash) {
     $formErrors.show();
   });
 },
+// TODO: consolidate addBuyOpenOrder and addSellOpenOrder
 addBuyOpenOrder(event) {
   event.preventDefault();
 
   let formData = this.getFormData();
-  console.log(formData);
 
   formData['buy'] = true;
   let correctQuote = this.quoteList.findWhere({symbol: formData['symbol']})
   formData['quote'] = correctQuote;
-  console.log(formData);
   const newOpenOrder = new OpenOrder(formData);
   if (newOpenOrder.isValid()) {
     this.model.add(newOpenOrder);
@@ -185,16 +113,12 @@ addSellOpenOrder(event) {
   event.preventDefault();
 
   let formData = this.getFormData();
-  console.log(formData);
 
   formData['buy'] = false;
-  // .where({symbol: quote.get('symbol')});
-  console.log('form data the symbol');
-  console.log(formData['symbol']);
+
   let correctQuote = this.quoteList.findWhere({symbol: formData['symbol']})
   formData['quote'] = correctQuote;
-  console.log('formData after adding the correct quote');
-  console.log(formData);
+
   const newOpenOrder = new OpenOrder(formData);
   // if is valid add newTask and clearformdata
   if (newOpenOrder.isValid()) {
@@ -208,17 +132,12 @@ addSellOpenOrder(event) {
   }
 },
 
-// break out into helper function
 getFormData() {
   console.log('in get form data');
   const openOrderData = {};
   ['symbol', 'price-target'].forEach((field) => {
     let val;
-    // const val =
-    // this.$(`.order-entry-form input[name=${field}]`).val();
-    console.log(field);
     if (field === 'symbol') {
-      console.log('in if statement if field')
       val =
       this.$(`.order-entry-form select[name=${field}]`).val();
     } else {
@@ -226,18 +145,11 @@ getFormData() {
       parseFloat(this.$(`.order-entry-form input[name=${field}]`).val());
       field = 'targetPrice';
     }
-    console.log(field);
-    // console.log('button buy or sell')
-    // console.log(this.$(`.order-entry-form button`).text())
-    console.log(val);
-    // const val = this.$( `.order-entry-form select[name=${field}] option:selected` ).text();
     if (val !== '') {
       openOrderData[field] = val;
     }
   });
-  console.log('openOrderData');
 
-  console.log(openOrderData);
   return openOrderData;
 },
 clearFormData () {
